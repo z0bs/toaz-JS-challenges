61cb814854024a15ab06b372e3f5c1f850d2969d
z0bs <anamschizoba@gmail.com>
2023-01-08 23:58:21 +0100

    part of challenge 4

---
+++ challenge5.js
@@ -0,0 +1,17 @@
+const bills = [125, 555, 44]
+
+const tip = []
+
+
+function calcTip (bill) {
+    // -------using the tenary operator--------
+
+      if (bill  > 50 && bill <= 300 bill / 0.15
+            ? console.log(`the bill was ${amount} and the tip was ${tip}`)
+            : tip > 300
+            ? amount / 0.2
+            : console.log(`the bill was ${amount} and the tip was ${tip}`))
+      : console.log(`No tip today`);
+  }
+  
+  calcTip(tip);
